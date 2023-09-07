2878bf1578dcc6d7db8c67f354f890ced02f53c4
Eric Yen <eric@ericyen.com>
2023-09-06 17:34:25 -0700

    update codeium

--- vim/codeium.nix
+++ vim/codeium.nix
@@ -4,7 +4,7 @@ pkgs.vimUtils.buildVimPluginFrom2Nix {
   src = pkgs.fetchFromGitHub {
     owner = "Exafunction";
     repo = "codeium.vim";
-    rev = "1.2.78";
+    rev = "1.2.84";
     hash = "sha256-SO0H0cXg0Pcmx4tvzRhtSQBgCvV11EUtYZ9vh+ZASAA=";
   };
 }
